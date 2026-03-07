ca70256a539584167b1d51725836b8fe5318bf80
kormuyang <Phantakan04@gmail.com>
2026-03-07 20:32:55 +0700

    update content

---
+++ materials/16-dynamic-programming/lectures/matrix_chan_multiplication-bottom_up.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX_N (int)(5e2)
+
+vector<vector<int>> a(MAX_N, vector<int>(2));
+vector<vector<int>> dp(MAX_N, vector<int>(MAX_N));
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i][0] >> a[i][1];
+    }
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 0;
+    }
+    for (int len = 1; len < n; len++) {
+        for (int i = 0; i + len < n; i++) {
+            int j = i + len;
+            dp[i][j] = INT_MAX;
+            for (int k = i; k < j; k++) {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + a[i][0] * a[k][1] * a[j][1]);
+            }
+        }
+    }
+    cout << dp[0][n - 1];
+    return 0;
+}
---
+++ materials/16-dynamic-programming/lectures/matrix_chan_multiplication-top_down.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX_N (int)(5e2)
+
+vector<vector<int>> a(MAX_N, vector<int>(2));
+vector<vector<int>> dp(MAX_N, vector<int>(MAX_N, -1));
+
+int mcm(int l, int r) {
+    if (dp[l][r] != -1) {
+        return dp[l][r];
+    }
+    if (l == r - 1) {
+        return dp[l][r] = a[l][0] * a[l][1] * a[r][1];
+    }
+    int ret = INT_MAX;
+    for (int i = l; i < r; i++) {
+        ret = min(ret, mcm(l, i) + mcm(i + 1, r) + a[l][0] * a[i][1] * a[r][1]);
+    }
+    return dp[l][r] = ret;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i][0] >> a[i][1];
+    }
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 0;
+    }
+    cout << mcm(0, n - 1);
+    return 0;
+}
